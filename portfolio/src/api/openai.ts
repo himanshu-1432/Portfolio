7794d414c604171b8c6882e74e8d76345dc586f5
AP\AryaHima <Himanshu.Arya@unisys.com>
2025-12-31 03:40:47 +0530

    server-api-added

--- portfolio/src/api/openai.ts
+++ portfolio/src/api/openai.ts
@@ -1,9 +1,36 @@
-export async function askOpenAI(message: string, history?: any[]) {
-  const res = await fetch("/api/chat", {
+// export async function askOpenAI(message: string) {
+//   const res = await fetch("http://localhost:5000/query", {
+//     method: "POST",
+//     headers: { "Content-Type": "application/json" },
+//     body: JSON.stringify({q:message}),
+//   });
+//   const data = await res.json();
+//   return data.answer as string;
+// }
+
+
+export async function askOpenAI(
+  query: string,
+  onToken: (chunk: string) => void
+) {
+  const res = await fetch("http://localhost:5000/query", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ message, history }),
+    body: JSON.stringify({ q: query })
   });
-  const data = await res.json();
-  return data.reply as string;
+
+  if (!res.body) {
+    throw new Error("No response body");
+  }
+
+  const reader = res.body.getReader();
+  const decoder = new TextDecoder("utf-8");
+
+  while (true) {
+    const { value, done } = await reader.read();
+    if (done) break;
+
+    const chunk = decoder.decode(value);
+    onToken(chunk); // token-by-token
+  }
 }
